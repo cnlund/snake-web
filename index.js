b1e87c8ed6a89bd11db9be779136e3967a91cbf0
cnlund <91978441+cnlund@users.noreply.github.com>
2024-08-05 00:32:01 -0500

    movimiento objeto

---
+++ index.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+    const pj = document.getElementById("personaje")
+    let pj_pos_x = 0
+    let pj_pos_y = 0
+    document.addEventListener("keydown", function(event) {
+        switch(event.key) {
+            case 'ArrowDown':
+                if(pj_pos_y < 95){
+                    pj_pos_y += 1
+                    pj.style.marginTop = pj_pos_y + 'vh'
+                    break;
+            }else{
+                break;
+            }
+            case 'ArrowUp':
+                if(pj_pos_y > 0){
+                    pj_pos_y -= 1
+                    pj.style.marginTop = pj_pos_y + 'vh'
+                    break;
+                }else{
+                    break;
+                }
+            case 'ArrowRight':
+                if(pj_pos_x < 96){
+                    pj_pos_x += 1
+                    pj.style.marginLeft = pj_pos_x + 'vw'
+                    break;
+                }else{
+                    break;
+                }
+            case 'ArrowLeft':
+                if(pj_pos_x > 0){
+                    pj_pos_x -= 1
+                    pj.style.marginLeft = pj_pos_x + 'vw' 
+                }         
+        }
+    })
+}
