e9469c5cff90132e158f72ff376647d95dde2398
cnlund <91978441+cnlund@users.noreply.github.com>
2024-08-08 05:07:28 -0500

    objeto y colision

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 window.onload = () => {
     const pj = document.getElementById("personaje")
+    const esfera = document.getElementById("punto")
     let pj_pos_x = 0
     let pj_pos_y = 0
+    let esfera_pos_x = 0
+    let esfera_pos_y = 0
+    function pos_esfera(){
+        esfera_pos_x = Math.floor(Math.random() * (96 - 0 + 1)) + 0
+        esfera_pos_y = Math.floor(Math.random() * (95 - 0 + 1)) + 0
+        esfera.style.display = 'block'
+        esfera.style.top = esfera_pos_y + 'vh'
+        esfera.style.left = esfera_pos_x + 'vw'
+    }
+    function colision_esfera(){
+        for(let i = 0; i<6; i++){
+            if(esfera_pos_x == pj_pos_x && esfera_pos_y == pj_pos_y){
+                esfera.style.display = 'none'
+                pos_esfera()
+            }if(esfera_pos_x+i == pj_pos_x && esfera_pos_y == pj_pos_y){
+                esfera.style.display = 'none'
+                pos_esfera()
+            }if(esfera_pos_x == pj_pos_x+i && esfera_pos_y == pj_pos_y){
+                esfera.style.display = 'none'
+                pos_esfera()
+            }if(esfera_pos_x+i == pj_pos_x+i && esfera_pos_y == pj_pos_y){
+                esfera.style.display = 'none'
+                pos_esfera()
+            }if(esfera_pos_x == pj_pos_x && esfera_pos_y+i == pj_pos_y){
+                esfera.style.display = 'none'
+                pos_esfera()
+            }if(esfera_pos_x == pj_pos_x && esfera_pos_y == pj_pos_y+i){
+                esfera.style.display = 'none'
+                pos_esfera()
+            }if(esfera_pos_x == pj_pos_x && esfera_pos_y+i == pj_pos_y+i){
+                esfera.style.display = 'none'
+                pos_esfera()
+            }
+        }
+    }
+    pos_esfera()
     document.addEventListener("keydown", function(event) {
         switch(event.key) {
             case 'ArrowDown':
@@ -34,5 +71,6 @@ window.onload = () => {
                     pj.style.left = pj_pos_x + 'vw' 
                 }         
         }
+        colision_esfera()
     })
 }
